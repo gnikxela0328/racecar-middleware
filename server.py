80542adb193312f2ab04e18e5ee4dcda9db9ead0
Alexander King <alex@freepazu.com>
2021-07-21 22:03:48 -0500

    addition of motors

--- server.py
+++ server.py
@@ -1,11 +1,18 @@
 # Imports
+import sys
+import time
 from flask import Flask
+from flask_cors import CORS
 from flask_restful import Resource, Api, reqparse
-#import qwiic_scmd
+import qwiic_scmd
+
+myMotor = qwiic_scmd.QwiicScmd()
 
 # Create API
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
+
 
 # Endpoints
 class Control(Resource):
@@ -18,30 +25,49 @@ class Control(Resource):
         parser = reqparse.RequestParser()
 
         parser.add_argument('controlType', required=True)
-        
+
         args = parser.parse_args()
-        
-        #print(args['controlType'])
-        
+
+        # print(args['controlType'])
+
         if(args['controlType'] == 'accel'):
-            print('forward')
-        if(args['controlType'] == 'stop'):
-            print('stop')
+            myMotor.set_drive(1, 0, 100)
+            myMotor.set_drive(0, 0, 100)
         if(args['controlType'] == 'reverse'):
-            print('back')
+            myMotor.set_drive(1, 1, 100)
+            myMotor.set_drive(0, 1, 100)
         if(args['controlType'] == 'left'):
-            print('left')
+            myMotor.set_drive(1, 1, 100)
+            myMotor.set_drive(0, 0, 100)
         if(args['controlType'] == 'right'):
-            print('right')
-        
-        
-        
+            myMotor.set_drive(1, 0, 100)
+            myMotor.set_drive(0, 1, 100)
+        if(args['controlType'] == 'stop'):
+            myMotor.set_drive(1, 0, 0)
+            myMotor.set_drive(0, 0, 0)
+
         return {'status': 'recieved'}, 200
     pass
+
+def init_motor():
+    if myMotor.connected ==  False:
+        print("Controller not connected")
+        return
+    myMotor.begin()
+    print("Controller initialized")
+    time.sleep(.250)
+    
+    myMotor.set_drive(0, 0, 0)
+    myMotor.set_drive(1, 0, 0)
+    
+    myMotor.enable()
+    print("Controller enabled")
+    time.sleep(.250)
 
 # Add route
 api.add_resource(Control, "/control")
 
 # Start
 if __name__ == '__main__':
-    app.run()
+    init_motor()
+    app.run(host="0.0.0.0")
